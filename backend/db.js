782ba9b0ca31ff8a9f23eea8e274fd963b70ed31
Ekansh <efuforofficial@gmail.com>
2024-05-10 13:34:19 +0530

    todo mongoose schema made

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('database connected')
+})
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean,
+})
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    todo
+}
